c1b83da2945f232a8d2d1682561c4b46eed264ca
jatinnn097 <jatinkumarsahu52@gmail.com>
2026-09-17 12:20:45 +0530

    Update MatrixMultiplication.java

--- MatrixMultiplication.java
+++ MatrixMultiplication.java
@@ -1,74 +1,133 @@
+import java.util.Random;
+
 class MatrixWorker extends Thread {
 
-    private int[][] a;
-    private int[][] b;
-    private int[][] answer;
+    private int[][] A;
+    private int[][] B;
+    private int[][] C;
     private int row;
+    private int col;
 
-    MatrixWorker(int[][] a, int[][] b, int[][] answer, int row) {
-        this.a = a;
-        this.b = b;
-        this.answer = answer;
+    MatrixWorker(int[][] A, int[][] B, int[][] C, int row, int col) {
+        this.A = A;
+        this.B = B;
+        this.C = C;
         this.row = row;
+        this.col = col;
     }
 
     public void run() {
-        for (int col = 0; col < b[0].length; col++) {
 
-            int sum = 0;
+        int sum = 0;
 
-            for (int k = 0; k < b.length; k++) {
-                sum = sum + a[row][k] * b[k][col];
-            }
-
-            answer[row][col] = sum;
+        for (int k = 0; k < 100; k++) {
+            sum = sum + A[row][k] * B[k][col];
         }
+
+        C[row][col] = sum;
     }
 }
 
-public class MatrixMultiplication {
+public class MatrixMultiplicationThreads {
+
+    static final int SIZE = 100;
 
     public static void main(String[] args) {
 
-        int n = 100;
+        int[][] A = new int[SIZE][SIZE];
+        int[][] B = new int[SIZE][SIZE];
+        int[][] C = new int[SIZE][SIZE];
 
-        int[][] first = new int[n][n];
-        int[][] second = new int[n][n];
-        int[][] product = new int[n][n];
+        Random random = new Random();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                first[i][j] = (i + j) % 10 + 1;
-                second[i][j] = (i * 2 + j) % 10 + 1;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                A[i][j] = random.nextInt(10);
+                B[i][j] = random.nextInt(10);
             }
         }
 
-        MatrixWorker[] workers = new MatrixWorker[n];
+        System.out.println("   MATRIX MULTIPLICATION USING THREADS");
+        System.out.println("Matrix A : 100 x 100");
+        System.out.println("Matrix B : 100 x 100");
+        System.out.println("Result   : 100 x 100");
+        System.out.println();
 
-        for (int i = 0; i < n; i++) {
-            workers[i] =
-                new MatrixWorker(first, second, product, i);
+        Thread[] threads = new Thread[SIZE * SIZE];
 
-            workers[i].start();
+        int index = 0;
+
+        for (int i = 0; i < SIZE; i++) {
+
+            for (int j = 0; j < SIZE; j++) {
+
+                threads[index] =
+                    new MatrixWorker(A, B, C, i, j);
+
+                threads[index].setName("T-" + index);
+
+                threads[index].start();
+
+                index++;
+            }
+
+            System.out.println("Processing row " + (i + 1) + " / 100");
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < threads.length; i++) {
+
             try {
-                workers[i].join();
+                threads[i].join();
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
         }
 
-        System.out.println("Matrix multiplication completed.");
+        System.out.println();
+        System.out.println("==========================================");
+        System.out.println("All threads completed!");
+        System.out.println("Total threads used: " + (SIZE * SIZE));
+        System.out.println("==========================================");
 
-        System.out.println("\nFirst 5 x 5 values of result:");
+        System.out.println();
+        System.out.println("MATRIX A (100 x 100)");
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print(product[i][j] + "\t");
+        for (int i = 0; i < SIZE; i++) {
+
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(A[i][j] + "\t");
             }
+
             System.out.println();
         }
+
+        System.out.println();
+        System.out.println("MATRIX B (100 x 100)");
+
+        for (int i = 0; i < SIZE; i++) {
+
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(B[i][j] + "\t");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("RESULT MATRIX C = A x B (100 x 100)");
+
+        for (int i = 0; i < SIZE; i++) {
+
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(C[i][j] + "\t");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("=");
+        System.out.println("Matrix Multiplication Completed!");
+        System.out.println("==");
     }
 }
